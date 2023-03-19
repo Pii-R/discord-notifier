f2017cfdb61941ec7b5fd12b9b85d5190f16eeba
Pierre <phenry.pm@gmail.com>
2023-03-19 23:27:51 +0100

    chore: missing file

--- src/messages/discord/handler.py
+++ src/messages/discord/handler.py
@@ -28,18 +28,6 @@ class DiscordHandler:
         self.client.run(self.settings.token)
 
 
-# @client.event
-# async def on_ready(self):
-#     user_id = "239145363041157122"  # Replace with the user ID you want to send a direct message to
-#     user = await self.client.fetch_user(user_id)
-#     dm_channel = await user.create_dm()
-#     message = "Hello, this is a direct message!"
-#     await dm_channel.send(message)
-#     await self.client.close()
-
-
-# client.run("MTA4Njk3MjMyMDYxMjA5NDAwMQ.GsboR-.I4O-a9duSi4Wm6omMsZZQhE7djFXKRZl35_V8M")
-
 # messages_to_send = {user1:[message, message1],user2:[message]}
 if __name__ == "__main__":
     settings = DiscordSettings(_env_file=".env.test", _env_file_encoding="utf-8")
